85834e6950be69217606504d223bee80ccc60285
GinMu <smu@anfengde.com>
2017-01-12 15:25:07 +0800

    modify items.py

--- employment/EmploymentSpider.py
+++ employment/EmploymentSpider.py
@@ -6,8 +6,15 @@ class EmploymentSpider(scrapy.Spider):
     allowed_domains = ["jobs.zhaopin.com","sou.zhaopin.com"]
     start_urls = ['http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E5%90%88%E8%82%A5&kw=%E5%89%8D%E7%AB%AF&sm=0&p=1']
     def parse(self, response):
-        salary = response.xpath('//td[@class="gsmc"]/a/text()').extract()
+        contents = response.xpath('//table[@class="newlist"][position()>1]')
+        # xpath解析table需去掉tbody
         item = EmploymentItem()
-        item['salary'] = salary
-        print(salary)
-        yield item
+        for content in contents:
+            item['title'] = content.xpath('.//tr/td[@class="zwmc"]/div/a/text()').extract()[0]
+            feedback = content.xpath('.//tr/td[@class="fk_lv"]/span/text()').extract()
+            item['feedback'] = len(feedback) != 0 and feedback[0] or ''
+            item['company'] = content.xpath('.//tr/td[@class="gsmc"]/a/text()').extract()[0]
+            item['salary'] = content.xpath('.//tr/td[@class="zwyx"]/text()').extract()[0]
+            item['location'] = content.xpath('.//tr/td[@class="gzdd"]/text()').extract()[0]
+            item['date'] = content.xpath('.//tr/td[@class="gxsj"]/span/text()').extract()[0]
+            yield item
--- employment/items.py
+++ employment/items.py
@@ -11,5 +11,9 @@ import scrapy
 class EmploymentItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    title = scrapy.Field()
+    feedback = scrapy.Field()
+    company = scrapy.Field()
     salary = scrapy.Field()
     location = scrapy.Field()
+    date = scrapy.Field()
